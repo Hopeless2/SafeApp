6a879f88b7daeb5b441b2bdc03f9feb397ae34cf
Hopeless2 <89778966354@mail.ru>
2022-05-17 22:25:55 +0300

    Commit init

--- src/main/java/com/example/safeapplication/controller/Controller.java
+++ src/main/java/com/example/safeapplication/controller/Controller.java
@@ -1,20 +1,39 @@
 package com.example.safeapplication.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @RequestMapping("/")
 @RestController
 public class Controller {
 
+
+    @Secured("ROLE_READ")
     @GetMapping("/hello")
     public String getHello() {
         return "hello";
     }
 
+    @RolesAllowed("ROLE_WRITE")
     @GetMapping("/secret-data")
     public String getSecretData() {
         return "secret data";
+    }
+
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
+    @GetMapping("/delete")
+    public String delete() {
+        return "delete";
+    }
+
+    @PreAuthorize("#username == authentication.principal.username")
+    @GetMapping("/username")
+    public String getName(String username) {
+        return "OK, username";
     }
 }
